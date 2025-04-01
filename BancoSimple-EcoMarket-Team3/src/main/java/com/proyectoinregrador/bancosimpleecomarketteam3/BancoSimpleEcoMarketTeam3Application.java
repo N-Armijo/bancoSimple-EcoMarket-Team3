90509b4a6c19fff6659fe412b26692270f5e2034
Shleideric <leonardo.gutial@gmail.com>
2025-04-01 15:57:14 -0300

    Inicio Proyecto Integrador

---
+++ BancoSimple-EcoMarket-Team3/src/main/java/com/proyectoinregrador/bancosimpleecomarketteam3/BancoSimpleEcoMarketTeam3Application.java
@@ -0,0 +1,13 @@
+package com.proyectoinregrador.bancosimpleecomarketteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSimpleEcoMarketTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoSimpleEcoMarketTeam3Application.class, args);
+	}
+
+}
---
+++ BancoSimple-EcoMarket-Team3/src/test/java/com/proyectoinregrador/bancosimpleecomarketteam3/BancoSimpleEcoMarketTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoinregrador.bancosimpleecomarketteam3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSimpleEcoMarketTeam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
